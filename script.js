e4663da4b09b97e9a1e6ef30d2e52cd5e1381cba
Abdullah Jawed <160762822+abdullahjawed25081997@users.noreply.github.com>
2024-11-24 18:57:34 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,54 @@
-function hamburg(){
-    const navbar = document.querySelector(".dropdown")
-    navbar.style.transform  = "translateY(0px)"
-}
-function cancel(){
-    const navbar = document.querySelector(".dropdown")
-    navbar.style.transform  = "translateY(-500px)"
-}
+AOS.init({ offset: 0 });
 
-// for Typewriter effect
-
+// Typewriter Effect
 const texts = [
     "DEVELOPER",
+    "GRAPHIC DESIGNER",
     "YOUTUBER",
     "DESIGNER"
-]
-
+];
 let speed = 100;
-
-const textElements = document.querySelector(".typewriter-text")
-
+const textElements = document.querySelector(".typewriter-text");
 let textIndex = 0;
 let charcterIndex = 0;
 
 function typeWriter() {
-    if(charcterIndex < texts[textIndex].length){
+    if (charcterIndex < texts[textIndex].length) {
         textElements.innerHTML += texts[textIndex].charAt(charcterIndex);
         charcterIndex++;
-        setTimeout(typeWriter, speed); 
-    }
-    else{
-        setTimeout(eraseText, 1000)
+        setTimeout(typeWriter, speed);
+    } else {
+        setTimeout(eraseText, 1000);
     }
 }
 
 function eraseText() {
-    if(textElements.innerHTML.length > 0){
-        textElements.innerHTML = textElements.innerHTML.slice(0,-1)
-        setTimeout(eraseText, 50)
-    }
-    else{
+    if (textElements.innerHTML.length > 0) {
+        textElements.innerHTML = textElements.innerHTML.slice(0, -1);
+        setTimeout(eraseText, 50);
+    } else {
         textIndex = (textIndex + 1) % texts.length;
         charcterIndex = 0;
-        setTimeout(typeWriter,500)
+        setTimeout(typeWriter, 500);
     }
 }
 
 window.onload = typeWriter;
 
+// View CV Function (to open the image in a new tab)
 function viewCV() {
     const link = document.createElement('a');
-    link.href = 'images/CV_new_format.jpg'; // Correct image path
+    link.href = 'images/CV_new_format.jpg'; // Ensure this is the correct path to your CV image
     link.target = '_blank'; // Open the image in a new tab
     link.click();
 }
 
-function hamburg(){
+function hamburg() {
     const navbar = document.querySelector(".dropdown");
-    navbar.style.transform  = "translateY(0px)";
+    navbar.style.transform = "translateY(0px)";
 }
-function cancel(){
+
+function cancel() {
     const navbar = document.querySelector(".dropdown");
-    navbar.style.transform  = "translateY(-500px)";
+    navbar.style.transform = "translateY(-500px)";
 }
